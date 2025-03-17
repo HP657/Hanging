ebef0d41d6476a530137c82b96cf7e120661c024
HP657 <junlee5070@gmail.com>
2025-03-18 00:44:18 +0900

    각 도메인 이름으로 ApiPath 생성

--- BackEnd/src/main/java/com/hp657/hanging/global/common/ApiPath.java
+++ BackEnd/src/main/java/com/hp657/hanging/global/common/ApiPath.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApiPath {
     public static final String BASE_PATH = "/api";
+    public static final String PROFESSOR_PATH = BASE_PATH + "/professor";
+    public static final String EVALUATION_PATH = BASE_PATH + "/evaluation";
+    public static final String SCOPE_PATH = BASE_PATH + "/scope";
+    public static final String VISITANT_PATH = BASE_PATH + "/visitant";
 
     public static String H2_PATH;
 
