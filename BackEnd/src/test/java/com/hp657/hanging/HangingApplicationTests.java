1f1cdb85c9eef9d4094b7567a137fd7bde6ce14f
HP657 <junlee5070@gmail.com>
2025-05-26 12:35:52 +0900

    빌드를 위해 Test 제거

--- BackEnd/src/test/java/com/hp657/hanging/HangingApplicationTests.java
+++ BackEnd/src/test/java/com/hp657/hanging/HangingApplicationTests.java
@@ -3,11 +3,11 @@ package com.hp657.hanging;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HangingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class HangingApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
